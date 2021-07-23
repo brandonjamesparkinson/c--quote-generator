44c278fcfaf3c41234bbf27425d741cc7035ff02
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2021-07-23 13:04:40 +0100

    + quote generator complete

--- RandomQuoteGenerator/Form1.Designer.cs
+++ RandomQuoteGenerator/Form1.Designer.cs
@@ -30,12 +30,53 @@ namespace RandomQuoteGenerator
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtRandomQuote = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnRandomQuote = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtRandomQuote
+            // 
+            this.txtRandomQuote.Location = new System.Drawing.Point(22, 102);
+            this.txtRandomQuote.Multiline = true;
+            this.txtRandomQuote.Name = "txtRandomQuote";
+            this.txtRandomQuote.Size = new System.Drawing.Size(469, 49);
+            this.txtRandomQuote.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnRandomQuote
+            // 
+            this.btnRandomQuote.Location = new System.Drawing.Point(155, 27);
+            this.btnRandomQuote.Name = "btnRandomQuote";
+            this.btnRandomQuote.Size = new System.Drawing.Size(188, 52);
+            this.btnRandomQuote.TabIndex = 2;
+            this.btnRandomQuote.Text = "Random Quote";
+            this.btnRandomQuote.UseVisualStyleBackColor = true;
+            this.btnRandomQuote.Click += new System.EventHandler(this.btnRandomQuote_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(515, 180);
+            this.Controls.Add(this.btnRandomQuote);
+            this.Controls.Add(this.txtRandomQuote);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtRandomQuote;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnRandomQuote;
     }
 }
 
--- RandomQuoteGenerator/Form1.cs
+++ RandomQuoteGenerator/Form1.cs
@@ -1,20 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RandomQuoteGenerator
 {
     public partial class Form1 : Form
     {
+        public string[] quotes =
+        {
+                "The way to get started is to quit talking and begin doing",
+                "If life were predictable it would cease to be life, and be without flavor.",
+                "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+                "Life is what happens when you're busy making other plans.",
+                "Spread love everywhere you go. Let no one ever come to you without leaving happier."
+        };
+
+        public string[] quoteAuthors =
+        {
+            "Jeremy",
+            "Phil",
+            "Bill",
+            "Ted",
+            "Jason"
+        };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRandomQuote_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int start2 = random.Next(0, quotes.Length);
+            txtRandomQuote.Text = quotes[start2] + " - " + quoteAuthors[start2];
         }
     }
 }
